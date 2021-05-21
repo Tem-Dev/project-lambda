997633267c569ca3bf1db1cb29d554f7f19b35a0
Ryuras <s.limaco@outlook.com>
2021-05-21 21:36:25 +0200

    design(ability-ui): design completed for a mock version

--- frontend/src/app/state/venture/module/player-combat-ui/component/ability-ui/ability-ui.component.ts
+++ frontend/src/app/state/venture/module/player-combat-ui/component/ability-ui/ability-ui.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Ability} from "../../../../service/player/player-state";
 
 @Component({
   selector: 'cp-ability-ui',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ability-ui.component.scss']
 })
 export class AbilityUiComponent implements OnInit {
+  @Input() ability: Ability;
 
   constructor() { }
 
