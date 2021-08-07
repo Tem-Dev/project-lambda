61c974878ef47ff76cb0d7faa7ca61e1e421df90
Tem-Dev <tem.dvlpr@gmail.com>
2021-08-07 22:51:08 +0200

    Refactor

--- backend/src/main/java/dev/collisionpoint/lambdabackend/CharacterController.java
+++ backend/src/main/java/dev/collisionpoint/lambdabackend/CharacterController.java
@@ -3,26 +3,27 @@ package dev.collisionpoint.lambdabackend;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/characters")
 public class CharacterController {
 
     private final CharacterRepository repository;
 
-    CharacterController(CharacterRepository repository) {
+    public CharacterController(CharacterRepository repository) {
         this.repository = repository;
     }
 
-    @PostMapping("/characters")
-    Character createCharacter(@RequestBody Character newCharacter) {
+    @PostMapping()
+    public Character createCharacter(@RequestBody Character newCharacter) {
         return repository.save(newCharacter);
     }
 
-    @GetMapping("/characters/{id}")
-    Character getCharacterById(@PathVariable Long id) {
+    @GetMapping("/{id}")
+    public Character getCharacterById(@PathVariable Long id) {
         return repository.findById(id).orElseThrow(() -> new CharacterNotFoundException(id));
     }
 
-    @PutMapping("/characters/{id}")
-    Character updateCharacter(@RequestBody Character newCharacter, @PathVariable Long id) {
+    @PutMapping("/{id}")
+    public Character updateCharacter(@RequestBody Character newCharacter, @PathVariable Long id) {
     return repository.findById(id)
             .map(character -> {
                 character.setName(newCharacter.getName());
@@ -34,8 +35,8 @@ public class CharacterController {
             });
     }
 
-    @DeleteMapping("/characters/{id}")
-    void deleteCharacter(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    public void deleteCharacter(@PathVariable Long id) {
         repository.deleteById(id);
     }
 }
