e68877671c911d150ea39d587d368fbc65d49bc5
Tem-Dev <tem.dvlpr@gmail.com>
2021-08-07 18:45:17 +0200

    Initial setup with spring initializr

---
+++ backend/src/main/java/dev/collisionpoint/lambdabackend/LambdaBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.collisionpoint.lambdabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaBackendApplication.class, args);
+	}
+
+}
